c5971ae8a424846b6759457d6cf547d5880f0706
Daniel Agbaji <daniel.agbaji@finlocker.com>
2021-06-28 13:46:06 -0500

    Initial Commit

---
+++ PracticeApi/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using PracticeApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeApi.Data
+{
+    public class DataContext:DbContext
+    {
+
+        public DataContext(DbContextOptions options) : base (options)
+        {
+           
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ PracticeApi/Entities/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApi.Entities
+{
+    public class AppUser
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public byte PasswordHash { get; set; }
+
+        public byte PasswordSaltHash { get; set; }
+    }
+}
